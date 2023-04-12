d84f0a16f0140f5a59a2668259d6b0ca6c7ec705
edoatleynewday <ed.oatley@newday.co.uk>
2023-04-12 16:53:28 +0100

    correct variable to private_ip_address

--- outputs.tf
+++ outputs.tf
@@ -36,5 +36,5 @@ output "YEDIS" {
 output "nodes" {
   sensitive = false
   description = "Detail the node name and private IP addresses"
-  value = { for node in azurerm_virtual_machine.YugaByte-Node : node.name => node.address_prefixes }
+  value = { for node in azurerm_virtual_machine.YugaByte-Node : node.name => node.private_ip_address }
 }
